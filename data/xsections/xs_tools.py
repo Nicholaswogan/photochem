09e4e3971934fbc97c672fd5c8ea510818dd42c5
Nicholaswogan <wogan@uw.edu>
2021-04-26 22:52:05 -0700

    improved xs consistency check. Added ranjan (2020) to H2O xs. Changed xs file format so temperature dependence is allowed

--- data/xsections/xs_tools.py
+++ data/xsections/xs_tools.py
@@ -1,3 +1,4 @@
+import numpy as np
 import os
 from ruamel.yaml import YAML
 yaml = YAML(typ='safe')
@@ -21,20 +22,114 @@ def check_consistency(verbose=True):
         for rx in meta_data[species]['reactions'].keys():
             if not os.path.isfile(species+'/'+rx.replace(' ','_')+'.txt'):
                 raise Exception('Reaction '+rx+' in xsections meta-data does not have a corresponding data file.')
-    
+
     directories = [a for a in os.listdir() if os.path.isdir(a) and a[0]!='.']
     try:
         directories.remove('__pycache__')
     except:
         pass
-    
+
     if set(all_species) != set(directories):
         bad = list(set(all_species).symmetric_difference(set(directories)))
         bad_str = ''
         for b in bad:
             bad_str += ' "'+b+'"'
         raise Exception('The folder(s)'+bad_str+' are not in the meta-data file.')
-        
+
+    # now read in data, and make sure it is sensible
+    for spec in meta_data.keys():
+        # get the xs
+        fil = open(spec+'/'+spec+'_xs.txt','r')
+        lines = fil.readlines()
+        fil.close()
+
+        ntemps = len(lines[1].split())-1
+        wv_xs = []
+        xs = []
+
+        for i,line in enumerate(lines[2:]):
+            wv_xs.append(float(line.split()[0]))
+            tmp = []
+            for j in range(1,ntemps+1):
+                tmp.append(float(line.split()[j]))
+            xs.append(np.array(tmp))
+
+        wv_qys = []
+        qys = []
+        qy_temps = []
+        for rx in meta_data[spec]['reactions']:
+            rxname = '_'.join(rx.split())
+            fil = open(spec+'/'+rxname+'.txt','r')
+            lines = fil.readlines()
+            fil.close()
+
+            ntemps = len(lines[1].split())-1
+            wv_qy = []
+            qy = []
+            for line in lines[2:]:
+                tmp = []
+                wv_qy.append(float(line.split()[0]))
+                for j in range(1,ntemps+1):
+                    tmp.append(float(line.split()[j]))
+                qy.append(tmp)
+            wv_qys.append(wv_qy)
+            qys.append(qy)
+
+            qy_temps.append([float(a.strip('K')) for a in lines[1].strip().split()[1:]])
+
+
+        nr = len(meta_data[spec]['reactions'])
+        for i in range(1,nr):
+            if len(qys[0][0]) != len(qys[i][0]):
+                raise Exception('The quantum yields for '+sp+' must all have the same number of temperature columns, with the same corresponding temperature')
+
+        for i in range(1,nr):
+            for j in range(len(qy_temps[0])):
+                if qy_temps[0][j] != qy_temps[i][j]:
+                    raise Exception('The quantum yields for '+sp+' have data at different temperatures.\n'+\
+                                    'This is not allowed.')
+
+        for i in range(1,nr):
+            if not np.isclose(np.min(wv_qys[0]),np.min(wv_qys[i])):
+                raise Exception('Wavelengths for the quantum yields for ',sp,' have different lower bounds. They must be the same.')
+            if not np.isclose(np.max(wv_qys[0]),np.max(wv_qys[i])):
+                raise Exception('Wavelengths for the quantum yields for ',sp,' have different upper bounds. They must be the same.')
+        if not np.isclose(np.min(wv_qys[0]),np.min(wv_xs)):
+            raise Exception('Wavelengths for the quantum yields and cross sections for ',sp,' have different lower bounds. They must be the same.')
+        if not np.isclose(np.max(wv_qys[0]),np.max(wv_xs)):
+            raise Exception('Wavelengths for the quantum yields and cross sections for ',sp,' have different upper bounds. They must be the same.')
+
+        # check qys add to less than 1
+        wv_new = np.linspace(np.min(wv_xs),np.max(wv_xs),1000)
+        qy_tot = np.zeros(1000)
+
+        for i in range(len(qy_temps[0])):
+            qy_tot = np.zeros(1000)
+            for j in range(len(qy_temps)):
+                qy_tot += np.interp(wv_new, wv_qys[j], np.array(qys[j])[:,i])
+
+        if not all(qy_tot<1.01):
+            raise Exception('Quantum yields sum for ',sp,' sum to >1. They should sum to <= 1.')
+
+        # check meta-data wv citations align
+        for rx in meta_data[spec]['reactions']:
+            low = []
+            high = []
+            for citation in meta_data[spec]['reactions'][rx]['citations']:
+                tmp = [float(a) for a in citation['nm-range'].split('-')]
+                low.append(tmp[0])
+                high.append(tmp[1])
+            if not np.isclose(np.min(low),np.min(wv_qys[0])) or not np.isclose(np.max(high),np.max(wv_qys[0])):
+                raise Exception('The citation range in the meta-data does not align with the data for reaction '+rx)
+        low = []
+        high = []
+        for citation in meta_data[spec]['xsections']['citations']:
+            tmp = [float(a) for a in citation['nm-range'].split('-')]
+            low.append(tmp[0])
+            high.append(tmp[1])
+        if not np.isclose(np.min(low),np.min(wv_xs)) or not np.isclose(np.max(high),np.max(wv_xs)):
+                raise Exception('The citation range in the meta-data does not align with the xs data for '+sp)
+
     if verbose:
         print('xs_metadata.yaml is consistent with the data files.')
 
@@ -54,11 +149,11 @@ def generate_photo_yaml_entries(species_list):
     for species in photo_species:
         for rx in meta_data[species]['reactions'].keys():
             tmp = set([a.strip() for a in rx.split('=>')[1].split('+')])
-            if tmp.issubset(species_set):        
+            if tmp.issubset(species_set):
                 rx_list.append({})
                 rx_list[-1]['reaction'] = rx
                 rx_list[-1]['type'] = 'photolysis'
     return rx_list
-    
+
 if __name__ == "__main__":
     check_consistency()
--- plan.md
+++ plan.md
@@ -10,46 +10,9 @@ Problem with PhotochemPy
 
 
 
+general notes
+- we need a routine which checks for non-linearities in short-lived species. If there are non-linearities, then stop the program.
 
-I think I understand the production rates. But i'm still worried about double counting reactions for stuff like H + H => H2. haha no double counting makes sense!
 
-```fortran
-np = nump(k) ! k is a species
-! np is number of reactions that produce species k
-do i=1,np
-  m = iprod(i,k) ! m is reaction number
-  l = numreactants(m) ! l is the number of reactants
-  do ii = 1,l
-    sp_inds(ii) = jchem(ii,k)
-  enddo
-  do iii = 1,nz
-    DD = 1.d0
-    do iiii = 1,l
-      DD = DD * D(sp_inds(iiii),iii)
-    enddo
-    xp(iii) = xp(iii) + A(iii,k) * DD
-  enddo
-enddo
-```
 
-What about loss rates? We can mirror above. This will make `xl` in units of molecules/cm3/s. This is different than PhotochemPy
-
-```fortran
-nl = numl(k) ! k is a species
-! nl is number of reactions that destroy species k
-do i=1,nl
-  m = iloss(i,k) ! This will JUST be reaction number
-  l = numreactants(m) ! number of reactants
-  do ii = 1,l
-    sp_inds(ii) = jchem(ii,k)
-  enddo
-  do iii = 1,nz
-    DD = 1.d0
-    do iiii = 1,l
-      DD = DD * D(sp_inds(iiii),iii)
-    enddo
-    xl(iii) = xl(iii) + A(iii,k) * DD
-  enddo
-enddo
-```
-
+<!--  -->
