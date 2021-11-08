10f544dd5b4b1749575d7a846726a5305ac4a702
Nicholas Wogan <wogan@uw.edu>
2021-11-08 10:45:44 -0800

    removed fortran-yaml-cpp, moving to fotran-yaml-c. Also using submodules

--- src/dependencies/cminpack/cminpack_wrapper.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "cminpack.h"
-
-void hybrd1c_wrapper(int (*fcn)(void *p, int n, const double *x, double *fvec, int iflag), 
-						void* p, int* n, double* x, double* fvec, double* tol, int* info, double* wa,
-						int* lwa)
-{
-  *info = __cminpack_func__(hybrd1)(fcn, p, *n, x, fvec, *tol, wa, *lwa);
-}
