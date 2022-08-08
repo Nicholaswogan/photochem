9ebd3481db188811153b49732a749165bd6144fb
Nick Wogan <nicholaswogan@gmail.com>
2022-08-07 20:33:24 -0700

    testevo revert

--- tests/testevo.f90
+++ tests/testevo.f90
@@ -1,4 +1,5 @@
 program testevo
+  use futils, only: linspace
   use photochem, only: EvoAtmosphere, version, dp
   implicit none
   character(:), allocatable :: err
@@ -6,38 +7,33 @@ program testevo
   logical :: success
   integer :: i, j
   real(dp), allocatable :: t_eval(:)
-  real(dp) :: from, to
   
   print*,'photochem version == ',trim(version)
 
   call pc%init("../photochem/data", &
                "../photochem/data/reaction_mechanisms/zahnle_earth.yaml", &
-               "../templates/Hescape/settings.yaml", &
+               "../tests/testevo_settings.yaml", &
                "../templates/ModernEarth/Sun_now.txt", &
-               "../templates/Hescape/atmosphere.txt", &
+               "../templates/ModernEarth/atmosphere_ModernEarth.txt", &
                err)
   if (allocated(err)) then
     print*,trim(err)
     stop 1
   endif
 
-  pc%var%atol = 1.0e-25_dp
-  pc%P_top_min = 1.0e-7_dp
-  pc%P_top_max = 1.0e100_dp
-  pc%top_atmos_adjust_frac = 0.05
+  ! Just take 3 steps
+  pc%var%mxsteps = 3
 
-  allocate(t_eval(400))
-  from = 5
-  to = 15
-  do i = 1,size(t_eval)
-    t_eval(i) = from + (to - from)*(i-1)/(size(t_eval)-1)
-  enddo
+  allocate(t_eval(100))
+  call linspace(5.0_dp, 17.0_dp, t_eval)
   t_eval = 10.0_dp**t_eval
 
-  success = pc%evolve('../test16.dat', 0.0_dp, pc%var%usol_init, t_eval, .true., err)
+  success = pc%evolve('testevo.dat', 0.0_dp, pc%var%usol_init, t_eval, .true., err)
   if (allocated(err)) then
     print*,trim(err)
     stop 1
   endif
+
+  deallocate(t_eval)
 
 end program
