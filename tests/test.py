b5e4ef60f1454ff07a61daa35c540bc0269e0e19
Nick Wogan <nicholaswogan@gmail.com>
2023-04-26 17:22:42 -0700

    fixed tests a bit. changed CI

--- tests/test.py
+++ tests/test.py
@@ -1,36 +1,43 @@
 from photochem import Atmosphere, zahnle_earth
 
-pc = Atmosphere(zahnle_earth,\
-                "../templates/ModernEarth/settings_ModernEarth.yaml",\
-                "../templates/ModernEarth/Sun_now.txt",\
-                "../templates/ModernEarth/atmosphere_ModernEarth.txt")
+def main():
 
-pc.initialize_stepper(pc.wrk.usol)
-tn = 0
-while tn<1e17:
-    tn = pc.step()
-pc.destroy_stepper()
+    pc = Atmosphere(zahnle_earth,\
+                    "../templates/ModernEarth/settings_ModernEarth.yaml",\
+                    "../templates/ModernEarth/Sun_now.txt",\
+                    "../templates/ModernEarth/atmosphere_ModernEarth.txt")
 
-con = pc.redox_conservation()
-print('redox conservation = '+'%.2e'%con)
-assert con < 1e-4
+    pc.initialize_stepper(pc.wrk.usol)
+    tn = 0
+    while tn < 1e17:
+        tn = pc.step()
+    pc.destroy_stepper()
 
-con = pc.atom_conservation('C').factor
-print('C conservation = '+'%.2e'%con)
-assert con < 1e-6
+    con = pc.redox_conservation()
+    print('redox conservation = '+'%.2e'%con)
+    assert con < 1e-4
 
-con = pc.atom_conservation('S').factor
-print('S conservation = '+'%.2e'%con)
-assert con < 1e-6
+    con = pc.atom_conservation('C').factor
+    print('C conservation = '+'%.2e'%con)
+    assert con < 1e-6
 
-con = pc.atom_conservation('H').factor
-print('H conservation = '+'%.2e'%con)
-assert con < 1e-6
+    con = pc.atom_conservation('S').factor
+    print('S conservation = '+'%.2e'%con)
+    assert con < 1e-6
 
-con = pc.atom_conservation('O').factor
-print('O conservation = '+'%.2e'%con)
-assert con < 1e-6
+    con = pc.atom_conservation('H').factor
+    print('H conservation = '+'%.2e'%con)
+    assert con < 1e-6
 
-con = pc.atom_conservation('Cl').factor
-print('Cl conservation = '+'%.2e'%con)
-assert con < 1e-6
+    con = pc.atom_conservation('O').factor
+    print('O conservation = '+'%.2e'%con)
+    assert con < 1e-6
+
+    con = pc.atom_conservation('Cl').factor
+    print('Cl conservation = '+'%.2e'%con)
+    assert con < 1e-6
+
+    pc.out2atmosphere_txt('../atmosphere_ModernEarth.txt')
+
+if __name__ == "__main__":
+    main()
