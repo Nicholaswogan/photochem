79425a94d3c7b38bd56aa7859d19191ae0d3305a
Nick Wogan <nicholaswogan@gmail.com>
2023-05-26 14:24:12 -0700

    fixed problem copying all data to photochem install

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from skbuild import setup
-from setuptools import find_namespace_packages
 from os import path
 
 this_directory = path.abspath(path.dirname(__file__))
@@ -15,7 +14,7 @@ with open("CMakeLists.txt",'r') as fil:
             
 setup(
     name="photochem",
-    packages=find_namespace_packages(),
+    packages=['photochem','photochem.utils'],
     python_requires='>=3.6',
     version=version,
     license="GNU General Public License v3.0",
