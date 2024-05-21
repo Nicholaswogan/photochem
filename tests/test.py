68ca65b77ed9a37cd60e4c6364b6449ca601a69c
Nick Wogan <nicholaswogan@gmail.com>
2024-05-21 15:25:34 -0700

    python test includes evo

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,4 @@
-from photochem import Atmosphere, zahnle_earth
+from photochem import Atmosphere, EvoAtmosphere, zahnle_earth
 
 def main():
 
@@ -10,6 +10,15 @@ def main():
     pc.initialize_stepper(pc.wrk.usol)
     tn = pc.step()
     pc.destroy_stepper()
+
+    pc1 = EvoAtmosphere(zahnle_earth,\
+                    "testevo_settings2.yaml",\
+                    "../templates/ModernEarth/Sun_now.txt",\
+                    "../templates/ModernEarth/atmosphere_ModernEarth.txt")
+    
+    pc1.initialize_stepper(pc1.wrk.usol)
+    tn = pc1.step()
+    pc1.destroy_stepper()
 
 if __name__ == "__main__":
     main()
