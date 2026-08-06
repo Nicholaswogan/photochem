09a58ea5e7c06bd82fe0a27162f4ca0e6efc4351
Nick Wogan <nicholaswogan@gmail.com>
2026-08-06 06:57:31 -0700

    update python test

--- tests/test_python.py
+++ tests/test_python.py
@@ -25,7 +25,50 @@ def test_initialize_atmosphere_z_no_particles():
     assert pc.var.top_atmos == z[-1]
     assert np.all(pc.wrk.pressure_hydro > 0.0)
 
-def test_wrapper(pc):
+
+def test_initialize_atmosphere_z_particles():
+    pc = EvoAtmosphere(
+        zahnle_earth,
+        "../examples/ModernEarth/settings.yaml",
+        "../examples/ModernEarth/Sun_now.txt",
+        "../examples/ModernEarth/atmosphere.txt",
+        data_dir="../data",
+    )
+    z = np.array([0.0, 5.0e6, 1.0e7])
+    temperature = np.array([300.0, 240.0, 180.0])
+    edd = np.array([1.0e5, 1.0e6, 1.0e7])
+    species_names = pc.dat.species_names[:pc.dat.nq]
+    particle_names = species_names[:pc.dat.np]
+    dominant_gas = pc.dat.np + np.argmax(
+        pc.wrk.usol[pc.dat.np:pc.dat.nq, 0]
+    )
+    mix = {species_names[dominant_gas]: np.ones(z.size)}
+    radii = {particle_names[0]: np.full(z.size, 2.0e-5)}
+
+    pc.initialize_atmosphere_z(
+        z, temperature, edd, 1.0e6, mix, particle_radius=radii
+    )
+
+    assert pc.var.top_atmos == z[-1]
+    assert np.all(np.isfinite(pc.wrk.usol))
+    assert np.all(pc.wrk.pressure_hydro > 0.0)
+    assert np.all(np.isfinite(pc.wrk.density))
+    assert np.all(pc.wrk.density > 0.0)
+    assert set(mix).isdisjoint(particle_names)
+    assert np.allclose(pc.var.particle_radius[0], 2.0e-5)
+    assert np.allclose(pc.var.particle_radius[1:], 1.0e-5)
+
+
+def test_wrapper():
+
+    pc = EvoAtmosphere(
+        zahnle_earth,
+        "../examples/ModernEarth/settings.yaml",
+        "../examples/ModernEarth/Sun_now.txt",
+        "../examples/ModernEarth/atmosphere.txt",
+        data_dir="../data"
+    )
+    
     print(pc.dat.nq)
     print(pc.dat.np)
     print(pc.dat.ng)
@@ -73,8 +116,6 @@ def test_wrapper(pc):
     print(pc.var.verbose)
     print()
     print(pc.wrk.nsteps)
-    print(pc.wrk.t_history[0])
-    print(pc.wrk.mix_history[0,0,0])
     print(pc.wrk.longdy)
     print(pc.wrk.longdydt)
     print(pc.wrk.tn)
@@ -88,25 +129,6 @@ def test_wrapper(pc):
     print(pc.wrk.amean_grd[0,0])
     print(pc.wrk.optical_depth[0,0])
     print(pc.wrk.surf_radiance[0])
-
-    # Exercise species-keyed altitude initialization through the Python API.
-    z = np.array([0.0, 5.0e6, 1.0e7])
-    temperature = np.array([300.0, 240.0, 180.0])
-    edd = np.array([1.0e5, 1.0e6, 1.0e7])
-    species_names = pc.dat.species_names[:pc.dat.nq]
-    particle_names = species_names[:pc.dat.np]
-    usol = pc.wrk.usol
-    dominant_gas = pc.dat.np + np.argmax(usol[pc.dat.np:pc.dat.nq, 0])
-    mix = {species_names[dominant_gas]: np.ones(z.size)}
-    radii = {particle_names[0]: np.full(z.size, 2.0e-5)}
-    pc.initialize_atmosphere_z(
-        z, temperature, edd, 1.0e6, mix, particle_radius=radii
-    )
-    assert pc.var.top_atmos == z[-1]
-    assert np.all(pc.wrk.pressure_hydro > 0.0)
-    assert set(mix).isdisjoint(particle_names)
-    assert np.allclose(pc.var.particle_radius[0], 2.0e-5)
-    assert np.allclose(pc.var.particle_radius[1:], 1.0e-5)
 
     pc.prep_atmosphere(pc.wrk.usol)
     pc.out2atmosphere_txt('tmp.txt',overwrite=True)
@@ -127,6 +149,8 @@ def test_wrapper(pc):
     pc.update_vertical_grid(TOA_pressure=1e-7*1e6)
     pc.clear_press_temp_edd_profile()
     pc.initialize_stepper(pc.wrk.usol)
+    print(pc.wrk.t_history[0])
+    print(pc.wrk.mix_history[0,0,0])
     pc.step()
     converged = pc.check_for_convergence()
     pc.destroy_stepper()
@@ -140,14 +164,9 @@ def test_wrapper(pc):
 
 def main():
 
+    test_wrapper()
+    test_initialize_atmosphere_z_particles()
     test_initialize_atmosphere_z_no_particles()
-
-    pc1 = EvoAtmosphere(zahnle_earth,\
-                        "../examples/ModernEarth/settings.yaml",\
-                        "../examples/ModernEarth/Sun_now.txt",\
-                        "../examples/ModernEarth/atmosphere.txt",\
-                        data_dir="../data")
-    test_wrapper(pc1)
 
 if __name__ == "__main__":
     main()
